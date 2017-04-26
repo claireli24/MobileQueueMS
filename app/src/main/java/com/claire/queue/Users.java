826b24274676bf214b29aa636f01688961099a73
claireli24 <clairelii24@gmail.com>
2017-04-26 20:17:33 +0800

    Registration with QR Code Scanning

---
+++ app/src/main/java/com/claire/queue/Users.java
@@ -0,0 +1,25 @@
+package com.claire.queue;
+
+
+public class Users {
+
+    private String userUsername;
+    private String userPhoneNumber;
+
+    public Users(){
+
+    }
+
+    public Users(String userUsername, String userPhoneNumber) {
+        this.userUsername = userUsername;
+        this.userPhoneNumber = userPhoneNumber;
+    }
+
+    public String getUserUsername() {
+        return userUsername;
+    }
+
+    public String getUserPhoneNumber() {
+        return userPhoneNumber;
+    }
+}
